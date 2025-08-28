fec7f69742b24f5c04786f803b59f7fece782b09
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2025-08-28 07:38:31 -0400

    Day #335 - Project64 + DSA + JMS + Datacamp

---
+++ my-jms-glassfish-7/child-jms-glassfish-7/src/test/java/com/brainycode/lab01/message/model/MessageCustomProperties.java
@@ -0,0 +1,37 @@
+package com.brainycode.lab01.message.model;
+
+import com.brainycode.labxx.common.settings.CommonSettings;
+import jakarta.jms.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.Enumeration;
+import java.util.UUID;
+
+public class MessageCustomProperties {
+
+    @Test
+    public void test() throws JMSException {
+        ConnectionFactory connectionFactory = CommonSettings.getConnectionFactory();
+        Queue queue = CommonSettings.getDefaultQueue();
+
+        try (JMSContext jmsContext = connectionFactory.createContext()) {
+            JMSProducer jmsProducer = jmsContext.createProducer();
+            TextMessage message = jmsContext.createTextMessage("Message with a custom property");
+            message.setBooleanProperty("priorityUser", true);
+            message.setStringProperty("authToken", UUID.randomUUID().toString());
+            jmsProducer.send(queue, message);
+
+            JMSConsumer consumer = jmsContext.createConsumer(queue);
+
+            // Delay for testing purpose Only
+            TextMessage textMsg = (TextMessage) consumer.receive(1000);
+            Enumeration customProperties = textMsg.getPropertyNames();
+            while (customProperties.hasMoreElements()) {
+                System.out.println(customProperties.nextElement());
+            }
+            System.out.println("priorityUser: " + textMsg.getBooleanProperty("priorityUser"));
+            System.out.println("authToken: " + textMsg.getStringProperty("authToken"));
+            System.out.println("Received message: " + textMsg.getText());
+        }
+    }
+}
